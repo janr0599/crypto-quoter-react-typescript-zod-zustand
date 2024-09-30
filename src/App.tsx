e1d3a799afa0f5279a6c8a2077ab6bca104e2b00
janr0599 <javiernr0599@gmail.com>
2024-09-30 17:04:05 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="app-title">
+          Cryptocurrency <span>Quoter</span>
+        </h1>
+
+        <div className="content"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
